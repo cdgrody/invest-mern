6f7053de6425ed013aac6e03320c37542eda0a02
cdgrody <cdgrody@gmail.com>
2023-03-10 15:07:52 -0800

    failed attempt at rechart

--- src/components/ChartArea/ChartArea.jsx
+++ src/components/ChartArea/ChartArea.jsx
@@ -11,60 +11,57 @@ import {
   CartesianGrid,
 } from "recharts";
 
-// Sample chart data
-const pdata = [
-  {
-    name: "MongoDb",
-    student: 11,
-    fees: 120,
-  },
-  {
-    name: "Javascript",
-    student: 15,
-    fees: 12,
-  },
-  {
-    name: "PHP",
-    student: 5,
-    fees: 10,
-  },
-  {
-    name: "Java",
-    student: 10,
-    fees: 5,
-  },
-  {
-    name: "C#",
-    student: 9,
-    fees: 4,
-  },
-  {
-    name: "C++",
-    student: 10,
-    fees: 8,
-  },
+const data = [
+  { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
+  { name: "Page B", uv: 40, pv: 240, amt: 240 },
 ];
-
-const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }, { name: "Page B", uv: 40, pv: 240, amt: 240 }];
-
-// const renderLineChart = (
-//   <LineChart width={400} height={400} data={data}>
-//     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-//   </LineChart>
-// );
 
 export default function App() {
   return (
-    <ResponsiveContainer id="chart-container" width="50%" height="50%" aspect={4}>
-      <LineChart data={pdata}  x={40} y={100}>
-        {/* <CartesianGrid /> */}
-        <XAxis dataKey="name" interval={"preserveStartEnd"} />
-        <YAxis></YAxis>
-        {/* <Legend /> */}
-        <Tooltip />
-        <Line dataKey="student" stroke="yellow" activeDot={{ r: 8 }} />
-        <Line dataKey="fees" stroke="red" activeDot={{ r: 8 }} />
-      </LineChart>
-    </ResponsiveContainer>
+    <>
+      <div className="chart-ctr" style={{ overflowX: "hidden" }}>
+        <ResponsiveContainer width="80%" aspect={4.5}>
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 15,
+              right: 600,
+              bottom: 0,
+              left: -400,
+            }}
+            responsive={true}
+          >
+            <CartesianGrid horizontal="true" vertical="" stroke="#243240" />
+            <XAxis dataKey="name" tick={{ fill: "#fff" }} />
+            <YAxis tick={{ fill: "#fff" }} />
+            <Tooltip
+              contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }}
+              itemStyle={{ color: "#fff" }}
+              cursor={false}
+            />
+            <Line
+              type="monotone"
+              dataKey="Iphone"
+              stroke="#8884d8"
+              strokeWidth="5"
+              dot={{
+                fill: "#2e4355",
+                stroke: "#8884d8",
+                strokeWidth: 2,
+                r: 5,
+              }}
+              activeDot={{
+                fill: "#2e4355",
+                stroke: "#8884d8",
+                strokeWidth: 5,
+                r: 10,
+              }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </>
   );
 }
