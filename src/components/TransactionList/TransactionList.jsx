6b9270c593d49bac1321245516d5db348e2d8fa0
cdgrody <cdgrody@gmail.com>
2023-03-10 09:30:30 -0800

    The transaction list correctly rerenders in the correct order when a new transaction is submitted

--- src/components/TransactionList/TransactionList.jsx
+++ src/components/TransactionList/TransactionList.jsx
@@ -1,8 +1,22 @@
+import { useState, useEffect } from 'react'
 import "./TransactionList.css";
 import TransactionCard from "../TransactionCard/TransactionCard";
 import HoldingsTable from "../HoldingsTable/HoldingsTable";
+import { getTransactions } from '../../utilities/transactions-api';
 
 export default function TransactionList({ user, transactions }) {
+
+  const [transactionList, setTransactionList] = useState(transactions)
+
+  useEffect(() => {
+    async function updateTransactionList() {
+      const transactionList = await getTransactions();
+      console.log(transactionList)
+      setTransactionList(transactionList);
+    }
+    updateTransactionList();
+  }, [transactions]);
+
   return (
     <>
       <div className="list-header">
@@ -12,7 +26,7 @@ export default function TransactionList({ user, transactions }) {
         </div>
         <HoldingsTable transactions={transactions}/>
       <div className="transaction-list-ctr">
-        {transactions.map((transaction, idx) => (
+        {transactionList.map((transaction, idx) => (
           <TransactionCard transaction={transaction} key={idx} />
         ))}
       </div>
