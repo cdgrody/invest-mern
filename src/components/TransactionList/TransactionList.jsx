cc7113245dec69276f66199f6c6058a9f398ad80
cdgrody <cdgrody@gmail.com>
2023-03-10 13:07:53 -0800

    Holdings table formatted to look nice

--- src/components/TransactionList/TransactionList.jsx
+++ src/components/TransactionList/TransactionList.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import "./TransactionList.css";
 import TransactionCard from "../TransactionCard/TransactionCard";
 import HoldingsTable from "../HoldingsTable/HoldingsTable";
-import { getTransactions } from '../../utilities/transactions-api';
+import { getTransactions } from "../../utilities/transactions-api";
 
 export default function TransactionList({ user, transactions }) {
-
-  const [transactionList, setTransactionList] = useState(transactions)
+  const [transactionList, setTransactionList] = useState(transactions);
+  const [showOptions, setShowOptions] = useState(false);
 
   useEffect(() => {
     async function updateTransactionList() {
@@ -16,19 +16,28 @@ export default function TransactionList({ user, transactions }) {
     updateTransactionList();
   }, [transactions]);
 
+  function handleClick(evt) {
+    setShowOptions(!showOptions)
+  }
+
   return (
     <>
       <div className="list-header">
-        <div>Transactions </div> 
-        <div>|</div> 
-        <div>Holdings </div>
-        </div>
-        <HoldingsTable transactions={transactions}/>
+        <div onClick={handleClick} className="list-header-el" id={showOptions ? "show-option" : ""}>Transactions </div>
+        <div onClick={handleClick} className="list-header-el" id={showOptions ? "" : "show-option"}>Holdings </div>
+      </div>
+{
+  !showOptions ?
+      <div className="holdings-table-ctr">
+        <HoldingsTable transactions={transactions} />
+      </div>
+      :
       <div className="transaction-list-ctr">
         {transactionList.map((transaction, idx) => (
           <TransactionCard transaction={transaction} key={idx} />
         ))}
       </div>
+      }
     </>
   );
 }
--- src/pages/OverviewPage/OverviewPage.jsx
+++ src/pages/OverviewPage/OverviewPage.jsx
@@ -13,7 +13,7 @@ export default function OverviewPage({
   return (
     <div className="main-body">
       <div className="chart-area">
-        <ChartArea />
+        {/* <ChartArea /> */}
       </div>
       <div className="bottom-half">
         <div className="left-area">
