733405ec5ac839adc5bfa598a5d8f5b34339d527
cdgrody <cdgrody@gmail.com>
2023-03-10 14:05:25 -0800

    Left area has been updated to show a few options including a new transaction form, crypto news, and AI investing

--- src/pages/OverviewPage/OverviewPage.jsx
+++ src/pages/OverviewPage/OverviewPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./OverviewPage.css";
 import TransactionForm from "../../components/TransactionForm/TransactionForm";
 import TransactionList from "../../components/TransactionList/TransactionList";
@@ -8,23 +9,54 @@ export default function OverviewPage({
   transactions,
   handleTransactionAdded,
   userBalances,
-  holdings
+  holdings,
 }) {
+  const [showTransactionForm, setShowTransactionForm] = useState(true);
+  const [showNews, setShowNews] = useState(true);
+  const [showAI, setShowAI] = useState(true);
+
+  function handleTransactionFormShow() {
+    setShowTransactionForm(!showTransactionForm)
+  }
+
+  function handleShowNews() {
+    setShowNews(!showNews)
+  }
+
+  function handleShowAI() {
+    setShowAI(!showAI)
+  }
+
   return (
     <div className="main-body">
-      <div className="chart-area">
-        {/* <ChartArea /> */}
-      </div>
+      <div className="chart-area">{/* <ChartArea /> */}</div>
       <div className="bottom-half">
         <div className="left-area">
-          <TransactionForm
-            user={user}
-            handleTransactionAdded={handleTransactionAdded}
-            userBalances={userBalances}
-          />
+          {showTransactionForm ? (
+            <>
+            <div className="transaction-form-preview" onClick={handleTransactionFormShow}>New Transaction ▶</div>
+            </>
+          ) : (
+            <>
+            <div className="transaction-form-preview" onClick={handleTransactionFormShow}>New Transaction ▼</div>
+            <div className="transaction-form">
+              <TransactionForm
+                user={user}
+                handleTransactionAdded={handleTransactionAdded}
+                userBalances={userBalances}
+                />
+            </div>
+                </>
+          )}
+            <div className="news-preview" onClick={handleShowNews}>Show Crypto News ▶</div>
+            <div className="ai-investing-advice-preview" onClick={handleShowAI}>AI Investing Advice ▶</div>
         </div>
         <div className="right-area">
-          <TransactionList user={user} transactions={transactions} holdings={holdings}/>
+          <TransactionList
+            user={user}
+            transactions={transactions}
+            holdings={holdings}
+          />
         </div>
       </div>
     </div>
