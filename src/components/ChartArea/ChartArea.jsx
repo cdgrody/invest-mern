72d770822d11610d5b569709cb3b7a9742619db1
cdgrody <cdgrody@gmail.com>
2023-03-13 12:40:59 -0700

    user networth and % change are calculated and displayed next to hte chart

--- src/components/ChartArea/ChartArea.jsx
+++ src/components/ChartArea/ChartArea.jsx
@@ -9,6 +9,8 @@ export default function ChartArea({holdings, userBalances, user}) {
   const [minDollarValue, setMinDollarValue] = useState(1000)
   const [maxDollarValue, setMaxDollarValue] = useState(1000)
   const [newUserBalance, setNewUserBalance] = useState(userBalances)
+  const [userNetWorth, setUserNetWorth] = useState(0)
+  const [percentChange, setPercentChange] = useState(0)
 
 
   useEffect(() => {
@@ -19,6 +21,8 @@ export default function ChartArea({holdings, userBalances, user}) {
       const maxDollarValue = Math.max(...data.map(item => item.dollars));
       setMinDollarValue(minDollarValue.toFixed(0))
       setMaxDollarValue(maxDollarValue.toFixed(0))
+      setUserNetWorth(data[data.length-1].dollars.toFixed(2))
+      setPercentChange((100*(data[data.length-1].dollars - data[0].dollars)/(data[data.length-1].dollars)).toFixed(2))
       setData(data)
     }
     collectData()
@@ -32,7 +36,15 @@ export default function ChartArea({holdings, userBalances, user}) {
     updateUserBalances();
   }, [user._id]);
 
+  function calculatePercentageChange() {
+    if(percentChange >= 0) return 'positive-percent' 
+    return 'negative-percent'
+  }
+
   return (
+    <>
+    <h1>${userNetWorth}</h1>
+    <h2 className={calculatePercentageChange}>{percentChange >= 0 ? `⬆${percentChange}` : `⬇${percentChange}`}</h2>
     <ResponsiveContainer width="80%" height="100%">
       <LineChart
         width={500}
@@ -53,6 +65,6 @@ export default function ChartArea({holdings, userBalances, user}) {
         <Line type="monotone" dataKey="dollars" stroke="gold" activeDot={{ r: 8 }} />
       </LineChart>
     </ResponsiveContainer>
-    
+    </>
   );
 }
