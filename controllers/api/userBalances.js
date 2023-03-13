59e2b51fcf000e860a98199c0c822e7bdf83224f
cdgrody <cdgrody@gmail.com>
2023-03-13 15:32:57 -0700

    If a user tries to buy more than their buying power, the controller will disregard the transaction

--- controllers/api/userBalances.js
+++ controllers/api/userBalances.js
@@ -28,6 +28,9 @@ async function index(req, res) {
 async function update(req, res) {
   try {
     const userBalance = await UserBalance.findById(req.params.id)
+    console.log('user balance', req.body.balance)
+    if(req.body.balance < 0) return res.status(400).json(err);
+    console.log('no user balance error', req.body.balance)
     const newBalance = req.body.balance;
     userBalance.balance = newBalance;
     await userBalance.updateOne({ balance: newBalance });
