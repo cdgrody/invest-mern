bdec2a9da40329c12dc997a75715d7a98f7cfef7
cdgrody <cdgrody@gmail.com>
2023-03-14 12:54:03 -0700

    News articles are styled and look cool

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,10 +1,15 @@
-import './NewsCard.css'
+import "./NewsCard.css";
 
-export default function NewsCard({newsStory}) {
-    return(
-        <>
-        <img className="news-image" src={newsStory.urlToImage}/>
-        <div>{newsStory.title}</div>
-        </>
-    )
+export default function NewsCard({ newsStory }) {
+  return (
+    <a href={newsStory.url} className="news-link" target="_blank" rel="noopener noreferrer">
+      <div className="news-card">
+        <img className="news-image" src={newsStory.urlToImage} alt={newsStory.title} />
+        <div className="news-details">
+          <div className="news-title">{newsStory.title}</div>
+          <div className="news-source">{newsStory.source.name}</div>
+        </div>
+      </div>
+    </a>
+  );
 }
