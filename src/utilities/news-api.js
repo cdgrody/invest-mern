920fc7f17038ec51e0310b88aa3ee4dead3beb3c
cdgrody <cdgrody@gmail.com>
2023-03-14 12:11:27 -0700

    A news api has been created

--- src/utilities/news-api.js
+++ src/utilities/news-api.js
@@ -1,1 +1,10 @@
 //api key 7fb617e482244143b760223dced04d71
+const API_KEY = "7fb617e482244143b760223dced04d71";
+
+export async function getNewsData() {
+  const response = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${API_KEY}`
+  );
+  const data = await response.json();
+  return data.slice(0, 5);
+}
