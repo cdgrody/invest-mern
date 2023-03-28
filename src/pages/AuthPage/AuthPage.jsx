ce0c6c52d3e3598a36782f21998edc5537e31c32
cdgrody <cdgrody@gmail.com>
2023-03-28 16:22:46 -0700

    A button bar is added to auth page for the user to select logging in, signing in, or continuing as guest

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,20 +1,45 @@
-import SignUpForm from '../../components/SignUpForm/SignUpForm'
-import LoginForm from '../../components/LoginForm/LoginForm'
-import NavBar from '../../components/NavBar/NavBar'
-import { ChartArea } from 'chart.js'
-import './AuthPage.css'
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import NavBar from "../../components/NavBar/NavBar";
+import { useState } from "react";
+import { ChartArea } from "chart.js";
+import "./AuthPage.css";
 
 export default function AuthPage({ setUser }) {
-    return (
-        <main>
-            {/* <ChartArea /> */}
-            <NavBar />
-            {/* <div className="auth-title">
+  const [logInState, setLogInState] = useState(-1);
+  const [signUpState, setSignUpState] = useState(-1);
+  const [guestState, setGuestState] = useState(-1);
+
+    function handleLoginClick() {
+        setSignUpState(-1)
+        setGuestState(-1)
+        setLogInState(-logInState)
+    }
+
+  return (
+    <>
+        <NavBar />
+        {/* <ChartArea /> */}
+        {/* <div className="auth-title">
             <h1>Welcome to Invest-MERN</h1>
             <h2>Earn, Learn, and Invest-MERN</h2>
-            </div> */}
-            <SignUpForm setUser={setUser} />
-            <LoginForm setUser={setUser} />
-        </main>
-    )
+        </div> */}
+        <div className="chart-area">Chart Area</div>
+        <div className="auth-buttons">
+          <div className="button-bar">
+            <div onClick={handleLoginClick}>Log In</div>
+            <div>Sign Up</div>
+            <div>Guest</div>
+          </div>
+          {logInState < 0? (
+              <></>
+          )
+          : (
+          <LoginForm setUser={setUser} /> 
+        )}
+          {/* <SignUpForm setUser={setUser} />
+        <LoginForm setUser={setUser} /> */}
+        </div>
+    </>
+  );
 }
