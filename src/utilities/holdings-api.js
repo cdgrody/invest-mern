eac4bc71913b4abb4afa8922f57d46e2f07881e7
cdgrody <cdgrody@gmail.com>
2023-04-26 01:04:07 -0400

    Minor cleanup for crypto-api

--- src/utilities/holdings-api.js
+++ src/utilities/holdings-api.js
@@ -3,13 +3,10 @@ const BASE_URL = '/api/holdings'
 
 export async function manageHolding(formData, holdings) {
     let createHolding = true
-    console.log('manage holdings', holdings, formData)
     for (let holding in holdings) {
         if(holdings[holding].asset.key === parseInt(formData.asset)) createHolding = false
     }
-    console.log('result', createHolding ? 'create holding' : 'update holding')
     if (createHolding) {
-        
         return sendRequest(BASE_URL, 'POST', formData)        
     } else {
         return sendRequest(`${BASE_URL}/${formData._id}`, 'PUT', formData)
